d3b9edb5e7b25f2967345a4388e8d921826eafa8
LauroPicasso <picasso1231@gmail.com>
2022-04-07 14:02:16 -0300

    Exercícios dia 04

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -0,0 +1,43 @@
+const a = 1;
+const b = 2;
+const c = 2;
+
+// /* Requisito 1 */
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+
+// /* Requisito 2 */
+// if (a > b) {console.log("Numéro maior é o A")}
+// else {console.log("Numéro maior é o B")}
+
+// /* Requisito 3 */
+// if (a > b && a > c) {console.log("Numéro maior é o A")}
+// else if (c > b) {console.log("Numéro maior é o C")}
+// else {console.log("Numéro maior é o B")}
+
+// /* Requisito 4 */
+// if (a > 0) {console.log("positive")}
+// else if (a < 0) {console.log("negative")}
+// else {console.log("zero")}
+
+// /* Requisito 5 */
+// const soma = a + b + c;
+// const angulosPositivos = a > 0 && b > 0 && c > 0;
+
+// if (angulosPositivos === true && soma === 180) {console.log(true)}
+// else if (soma < 0) {console.log("error")}
+// else {console.log(false)}
+
+/* Requisito 6 */
+let pecaXadres;
+
+switch (pecaXadres.toLowerCase() {
+    case bispo: console.log("diagonais")
+    break
+    case rei: console.log("apenas uma casa em qualquer direção")
+    case rei: console.log()
+}
+
