4bb7c85d10964081ca6588e2a18ba19f05772e70
LauroPicasso <picasso1231@gmail.com>
2022-04-07 21:04:28 -0300

    Bloco 04 | Exercícios dia 02

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/Exercício 08.js
@@ -0,0 +1,10 @@
+// Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;
+
+let numbers = []
+
+let contador = 0;
+
+for(let index = 0; index < numbers.length; index +=){
+    if(numbers[index] < contador)
+    }
+}
